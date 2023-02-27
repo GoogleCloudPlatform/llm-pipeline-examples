7a61087f09f01ec9911072be75ca16063e6943a6
Kuo Lin <kuolin@google.com>
2023-02-27 21:33:34 +0000

    update training cluster monitor description

--- src/orchestration/training_cluster_monitor.py
+++ src/orchestration/training_cluster_monitor.py
@@ -11,8 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Monitors training clusters. Restarts training cluster if detects training
-stall.
+"""Monitor training clusters.
+
+Restarts training cluster if detects training not progressing.
 """
 import os
 import re
