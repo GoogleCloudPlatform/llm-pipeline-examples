b5df25e013ce4f3b5f06b53ef951aa03cff445ed
Christopher Pirillo <pirillo.chris@gmail.com>
2023-02-28 11:10:23 -0800

    Missing files

--- scripts/fasttransformer/all_in_one.sh
+++ scripts/fasttransformer/all_in_one.sh
@@ -1,6 +1,7 @@
 # Arg1 - NumGpus
 # Arg2 - Model Path on HuggingFace
 # Arg3 - DSM for GPU
+# Arg4 - GCS path to upload the model
 ./faster_transformer_install.sh $3
 ./download_t5.sh $2
 
@@ -11,4 +12,8 @@ do
     MODEL_NAME = x
 done
 
-./convert_existing_t5.sh $1 $MODEL_NAME
+./convert_existing_t5.sh $1 $MODEL_NAME $(pwd)
+
+GCS_PATH=${$4/\/gcs\//gs:\/\/}
+
+gsutil cp -r ./$2 gs://$GCS_PATH/$2
