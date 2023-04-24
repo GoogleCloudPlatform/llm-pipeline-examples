33383b84ea903e02f83547b0d5d413217dd50dc9
Christopher Pirillo <pirillo@google.com>
2023-04-24 10:12:56 -0700

    Remove unused code

--- gke/run_batch.sh
+++ gke/run_batch.sh
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+EXIT_CODE = 0
 
 _invoke_cluster_tool () {
   echo "Invoking cluster tool"
@@ -52,27 +52,26 @@ if [[ -z $CLUSTER_ID ]]; then
   echo User is ${OS_LOGIN_USER}
 
   export ACTION=CREATE
-  #export IMAGE_NAME=c0-deeplearning-common-cu113-v20221026-debian-10
-  #export TERRAFORM_GCS_PATH=${MODEL_OUTPUT/\/gcs\//gs:\/\/}/deployment
   export METADATA="{install-unattended-upgrades=\"false\",enable-oslogin=\"TRUE\",jupyter-user=\"${OS_LOGIN_USER}\",install-nvidia-driver=\"True\"}"
   export SHOW_PROXY_URL=no
   export LABELS="{gcpllm=\"$CLUSTER_PREFIX\"}"
   export MINIMIZE_TERRAFORM_LOGGING=true
-  #export DISK_SIZE_GB=1000
   _invoke_cluster_tool
 
   echo "Provisioning cluster..."
-
   export CLUSTER_ID=${NAME_PREFIX}-gke
+  
+  gcloud container clusters get-credentials $CLUSTER_ID --region $REGION --project $PROJECT_ID
+  
+  # Apply Nvidia daemonset
+  kubectl apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/container-engine-accelerators/master/nvidia-driver-installer/cos/daemonset-preloaded.yaml
+
+  # TODO: need to create Kubernetes Service Account that is linked to Workload Identity
 fi
 
 # Get kubeconfig for cluster
 REGION=${ZONE%-*}
 gcloud container clusters get-credentials $CLUSTER_ID --region $REGION --project $PROJECT_ID
-
-#export GPU_NUMBER=4
-#export MODEL_SOURCE_PATH="gs://<idksomething>"
-#export MODEL_UPLOAD_PATH="gs://<idksomethingelse"
 
 # Run convert image on cluster
 export CONVERT_JOB_ID=convert-$RANDOM
@@ -81,55 +80,5 @@ kubectl wait --for=condition=complete --timeout=10m job/$CONVERT_JOB_ID
 
 # Run deploy-gke image on cluster
 envsubst < specs/inference.yml | kubectl apply -f -
-exit 0
-
-
-monitoring_started=false
-export EXIT_CODE=
-export LOG_START_TIME=$(date -Ins | sed -e "s/,/\./")
-while [[ -z "$EXIT_CODE" ]]; do
-  export RESULT=$(gsutil cat ${MODEL_OUTPUT/\/gcs\//gs:\/\/}/progress.txt 2> /dev/null)
-  if [[ "${RESULT}" == "succeeded" ]]; then
-    echo "Training finished successfully!"
-    export EXIT_CODE=0
-    break
-  fi
-  if [[ "${RESULT}" == "failed" ]]; then
-    echo "Training failed!"
-    export EXIT_CODE=1
-    break
-  fi
-
-  if [[ -e check.txt ]]; then
-    rm check.txt
-    
-    if [[ "${RESULT}" != "started" ]]; then
-      echo "Timeout! Training not started in 20 minutes"
-      export EXIT_CODE=2
-      break
-    fi
-  fi
-
-  if [[ "${RESULT}" == "started" && "$monitoring_started" != true  ]]; then
-    echo "Training started, start monitoring."
-    (python3 training_cluster_monitor.py --project_id=${PROJECT} --model_output=${MODEL_OUTPUT}) &
-    monitoring_started=true
-  fi
-
-  export LOG_END_TIME=$(date -Ins | sed -e "s/,/\./")
-  sleep 15
-  # Reading logs 15 seconds behind to give them a chance to be collected.
-  gcloud logging read "log_name=projects/${PROJECT}/logs/deepspeed labels.\"compute.googleapis.com/resource_name\"=~\"${JOB_ID}\" timestamp>=\"${LOG_START_TIME}\" timestamp<\"${LOG_END_TIME}\"" --project=${PROJECT} | yq -r .textPayload | tac
-  export LOG_START_TIME=${LOG_END_TIME}
-  
-done
-
-# Only delete cluster when training succeeds. Otherwise, keep the cluster for investigation
-if [[ "${EXIT_CODE}" == "0" ]]; then
-  #gcloud compute instance-groups managed delete ${JOB_ID} --quiet --project=${PROJECT} --zone=${ZONE}
-  #gcloud compute instance-templates delete ${JOB_ID} --quiet --project=${PROJECT}
-  export ACTION=DESTROY
-  _invoke_cluster_tool
-fi
 
 exit $EXIT_CODE
