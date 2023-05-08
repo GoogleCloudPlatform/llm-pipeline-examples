2a6b56a26583fe66ab59b480c65a6cd913e5dc57
Christopher Pirillo <pirillo@google.com>
2023-05-08 13:25:54 -0700

    Remove debug statements from js

--- src/app/static/infer.js
+++ src/app/static/infer.js
@@ -1,10 +1,5 @@
 var btn = document.getElementById("submit");
 
-const urlParams = new URLSearchParams(location.search);
-if (urlParams.has("fakedata")) {
-    var fakeData=true
-}
-
 btn.addEventListener('click', async function(event) {
     event.preventDefault();
     var input = document.getElementById("prompt").value;
@@ -14,21 +9,15 @@ btn.addEventListener('click', async function(event) {
     }
 
     var responseElement = document.getElementById("response")
-    var responsePromise = {}
-    if (fakeData) {
-        responsePromise = Promise.resolve({"metrics": {"preprocessing": 10, "inferencing": 30, "postprocessing": 2}, "predictions": ["'Tapi Tapi -- Handcrafted, authentic African ice cream' is a", "'Tapi Tapi -- Handcrafted, authentic African ice cream' is a", "'Tapi Tapi -- Handcrafted, authentic African ice cream' is a", "'Tapi Tapi -- Handcrafted, authentic African ice cream' is a", "'Tapi Tapi -- Handcrafted, authentic African ice cream' is a"]});
-    }
-    else {
-        responsePromise = fetch('/infer?metrics=true', {
-            method: 'POST',
-            body: JSON.stringify(input),
-            headers: {
-                "Content-Type": "application/json",
-            },
-            cache: "no-cache",
-        })
-        .then((response) => response.json());
-    }
+    var responsePromise = fetch('/infer?metrics=true', {
+        method: 'POST',
+        body: JSON.stringify(input),
+        headers: {
+            "Content-Type": "application/json",
+        },
+        cache: "no-cache",
+    })
+    .then((response) => response.json());
 
     var responseJson = await responsePromise;
     console.log(responseJson);
