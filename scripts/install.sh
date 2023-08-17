fdadf787107422812d3c5134f05d18d211eaab28
abdallag <abdallag@google.com>
2023-08-17 15:31:05 -0700

    checkout 2.0.1

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,8 +14,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-DEEPSPEED_PIP_VERSION='deepspeed<0.10'
-
 if [[ -z "$(pip list | grep deepspeed)" ]]; then
   ./clean_up_torch_xla.sh
   pip install 'datasets>=2.9.0' evaluate transformers
@@ -28,6 +26,6 @@ if [[ -z "$(pip list | grep deepspeed)" ]]; then
   pip install triton
   pip install deepspeed
   pip uninstall -y deepspeed
-  DS_BUILD_CPU_ADAM=1 DS_BUILD_FUSED_ADAM=1 DS_BUILD_FUSED_LAMB=1 DS_BUILD_TRANSFORMER=1 DS_BUILD_TRANSFORMER_INFERENCE=1 DS_BUILD_STOCHASTIC_TRANSFORMER=1 DS_BUILD_UTILS=1 pip install $DEEPSPEED_PIP_VERSION --global-option="build_ext"
+  DS_BUILD_CPU_ADAM=1 DS_BUILD_FUSED_ADAM=1 DS_BUILD_FUSED_LAMB=1 DS_BUILD_TRANSFORMER=1 DS_BUILD_TRANSFORMER_INFERENCE=1 DS_BUILD_STOCHASTIC_TRANSFORMER=1 DS_BUILD_UTILS=1 pip install deepspeed --global-option="build_ext"
 fi
 
