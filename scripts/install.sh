cd5d03ec15f7a5ed2c7878c173f9758867180ccd
abdallag <abdallag@google.com>
2023-11-17 16:59:34 -0800

    Install deepspeed dependencies to minimize search

--- scripts/install.sh
+++ scripts/install.sh
@@ -23,6 +23,7 @@ if [[ -z "$(pip list | grep deepspeed)" ]]; then
   pip install scipy
   pip install sentencepiece
   pip install triton
+  pip install aiohttp aiosignal async_timeout attrs charset_normalizer
   pip install deepspeed==0.12.3
   pip uninstall -y deepspeed
   DS_BUILD_OPS=1 DS_BUILD_UTILS=1 pip install deepspeed==0.12.3 --global-option="build_ext"
